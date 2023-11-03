7026daadce7249ef3a10e4693f400b7b8c6093c9
w4yZ <phiriyakornman@gmail.com>
2023-11-03 14:10:30 +0700

    user validation

---
+++ lib/validations/user.ts
@@ -0,0 +1,23 @@
+import * as z from "zod";
+
+export const UserValidation = z.object({
+  profile_photo: z.string().url().nonempty(),
+  //   profile_photo: z
+  //     .string({
+  //       required_error: "Name is required",
+  //       invalid_type_error: "Name must be a string",
+  //     })
+  //     .url(),
+  name: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(30, { message: "Maximum 30 caracters." }),
+  username: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(30, { message: "Maximum 30 caracters." }),
+  bio: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(1000, { message: "Maximum 1000 caracters." }),
+});
